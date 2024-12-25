cbfac6820021beb6df26bc23d8c0d31874499a7b
Rubén González <ruben.gonzalezgomez@gmail.com>
2024-12-25 20:44:45 +0100

    build index files

---
+++ client.ts
@@ -0,0 +1,6 @@
+'use client'
+
+export { default as BgColorFeatureClient } from './src/color/features/bgColor/feature.client';
+export { default as TextColorFeatureClient } from './src/color/features/textColor/feature.client';
+export { default as HighlightColorFeatureClient } from './src/color/features/highlightColor/feature.client';
+export { default as YoutubeFeatureClient } from './src/embed/features/youtube/feature.client';
---
+++ index.ts
@@ -0,0 +1,4 @@
+export { BgColorFeature } from './src/color/features/bgColor/feature.server';
+export { TextColorFeature } from './src/color/features/textColor/feature.server';
+export { HighlightColorFeature } from './src/color/features/highlightColor/feature.server';
+export { YoutubeFeature } from './src/embed/features/youtube/feature.server';
